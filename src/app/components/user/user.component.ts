4f64ea40e5ae15d3cc7b40679ab85446fe92d3f2
Monster <vongyuoyi@KPC-004.local>
2023-04-03 16:10:12 +0700

    reference only: do not follow

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+import { catchError, Observable, tap } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,33 +8,36 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  users: User[] = [];
+  users!: Observable<any>;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.fetchAllUsers().subscribe((res) => {
-      console.log(res.message);
-      this.users = res.payload;
-    });
+    this.users = this.userService.fetchAllUsers().pipe(
+      tap((a: any) => {
+        return a.payload;
+      }),
+      catchError((err) => {
+        return new Observable();
+      })
+    );
+    console.log(this.users);
   }
 
   sendTestUser() {
-    this.userService
-      .createUser({
-        username: 'dayan1',
-        email: 'eam.dayan1@gmail.com',
-        telephone: '089123124',
-        profile:
-          'https://angular.io/assets/images/logos/angular/logo-nav@2x.png',
-        roles: ['TEACHER'],
-      })
-      .subscribe((res) => {
-        console.log(res);
-        this.userService.fetchAllUsers().subscribe((res) => {
-          console.log(res.message);
-          this.users = res.payload;
-        });
-      });
+    this.userService.createUser({
+      username: 'dayan1',
+      email: 'eam.dayan1@gmail.com',
+      telephone: '089123124',
+      profile: 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png',
+      roles: ['TEACHER'],
+    });
+    // .subscribe((res) => {
+    //   console.log(res);
+    //   this.userService.fetchAllUsers().subscribe((res) => {
+    //     console.log(res.message);
+    //     this.users = res.payload;
+    //   });
+    // });
   }
 }
